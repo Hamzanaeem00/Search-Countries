025207a3f25381f8e42fdff9a1c146622154ed7e
Hamzanaeem00 <hamzanaeemkhan977@gmail.com>
2023-02-12 03:38:25 +0500

    reviews app

--- src/App.js
+++ src/App.js
@@ -1,14 +1,22 @@
 
 import './App.css';
+import Review from './Review';
 
 import Weather from './Wd/Weather';
 
 function App() {
   return (
-    <div className="App">
+    <main>
+      <section className='container'>
+        <div className='title'>
+          <h2>our reviews</h2>
+          <div className='underline'></div>
+        </div>
+        <Review />
+      </section>
+    </main>
+    
 
-    <Weather />
-    </div>
   );
 }
 
